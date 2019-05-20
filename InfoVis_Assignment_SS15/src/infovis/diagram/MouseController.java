8f18b8cf7c42a57f5bc17729048f946a549411ce
Quan <n.dinh@tdsoftware.de>
2019-05-20 19:42:51 +0200

    change draw technique and reformat

--- InfoVis_Assignment_SS15/src/infovis/diagram/MouseController.java
+++ InfoVis_Assignment_SS15/src/infovis/diagram/MouseController.java
@@ -1,4 +1,4 @@
- package infovis.diagram;
+package infovis.diagram;
 
 import infovis.debug.Debug;
 import infovis.diagram.elements.DrawingEdge;
@@ -8,8 +8,6 @@ import infovis.diagram.elements.GroupingRectangle;
 import infovis.diagram.elements.None;
 import infovis.diagram.elements.Vertex;
 import infovis.diagram.layout.Fisheye;
-import java.awt.geom.Rectangle2D;
-
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -18,75 +16,81 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class MouseController implements MouseListener,MouseMotionListener {
-	 private Model model;
-	 private View view;
-	 private Element selectedElement = new None();
-	 private double mouseOffsetX;
-	 private double mouseOffsetY;
-	 private boolean edgeDrawMode = false;
-	 private DrawingEdge drawingEdge = null;
-	 private boolean fisheyeMode;
-	 private GroupingRectangle groupRectangle;
-	 
-	 private boolean hitOverview = false; // overview
-	 private boolean hitMarker = false; // marker
-	 
-	 private Fisheye fisheye = new Fisheye(); // fisheye
-	 private Model fishModel; //fisheye
-	 
+public class MouseController implements MouseListener, MouseMotionListener {
+	private Model model;
+	private View view;
+	private Element selectedElement = new None();
+	private double mouseOffsetX;
+	private double mouseOffsetY;
+	private boolean edgeDrawMode = false;
+	private DrawingEdge drawingEdge = null;
+	private boolean fisheyeMode;
+	private GroupingRectangle groupRectangle;
+
+	private boolean hitTopOverview = false; // overview
+	private boolean hitMarker = false; // marker
+
+	private double currentX = 0; // mouse coordinate x
+	private double currentY = 0; // mouse coordinate y
+
+	private Fisheye fisheye = new Fisheye(); // fisheye
+	private Model fishModel; // fisheye
+
 	/*
 	 * Getter And Setter
 	 */
-	 public Element getSelectedElement(){
-		 return selectedElement;
-	 }
-    public Model getModel() {
+	public Element getSelectedElement() {
+		return selectedElement;
+	}
+
+	public Model getModel() {
 		return model;
 	}
+
 	public void setModel(Model diagramModel) {
 		this.model = diagramModel;
 	}
+
 	public View getView() {
 		return view;
 	}
+
 	public void setView(View diagramView) {
 		this.view = diagramView;
 	}
+
 	/*
-     * Implements MouseListener
-     */
+	 * Implements MouseListener
+	 */
 	public void mouseClicked(MouseEvent e) {
 		int x = e.getX();
 		int y = e.getY();
 		double scale = view.getScale();
-		
-		
-		
-		if (e.getButton() == MouseEvent.BUTTON3){
+
+		if (e.getButton() == MouseEvent.BUTTON3) {
 			/*
 			 * add grouped elements to the model
 			 */
-			Vertex groupVertex = (Vertex)getElementContainingPosition(x/scale,y/scale);
-			for (Iterator<Vertex> iter = groupVertex.getGroupedElements().iteratorVertices();iter.hasNext();){
+			Vertex groupVertex = (Vertex) getElementContainingPosition(x / scale, y / scale);
+			for (Iterator<Vertex> iter = groupVertex.getGroupedElements().iteratorVertices(); iter.hasNext();) {
 				model.addVertex(iter.next());
 			}
-			for (Iterator<Edge> iter = groupVertex.getGroupedElements().iteratorEdges();iter.hasNext();){
+			for (Iterator<Edge> iter = groupVertex.getGroupedElements().iteratorEdges(); iter.hasNext();) {
 				model.addEdge(iter.next());
 			}
 			/*
 			 * remove elements
 			 */
 			List<Edge> edgesToRemove = new ArrayList<Edge>();
-			for (Iterator<Edge> iter = model.iteratorEdges(); iter.hasNext();){
+			for (Iterator<Edge> iter = model.iteratorEdges(); iter.hasNext();) {
 				Edge edge = iter.next();
-				if (edge.getSource() == groupVertex || edge.getTarget() == groupVertex){
+				if (edge.getSource() == groupVertex || edge.getTarget() == groupVertex) {
 					edgesToRemove.add(edge);
 				}
 			}
 			model.removeEdges(edgesToRemove);
 			model.removeElement(groupVertex);
-			
+
 		}
 	}
 
@@ -95,97 +99,101 @@ public class MouseController implements MouseListener,MouseMotionListener {
 
 	public void mouseExited(MouseEvent arg0) {
 	}
+
 	public void mousePressed(MouseEvent e) {
 		int x = e.getX();
 		int y = e.getY();
 		double scale = view.getScale();
-		
-		int xOver=(int) (view.overviewX * view.overviewScale * scale);
-		int yOver=(int) (view.overviewY * view.overviewScale * scale);
-		int wOver=(int) (view.overviewWidth * view.overviewScale * scale);
-		int hOver=(int) (view.overviewHeight * view.overviewScale * scale);
-		
-		int xMark=(int) (view.markerX * view.overviewScale * scale);
-		int yMark=(int) (view.markerY * view.overviewScale * scale);
-		int wMark=(int) (view.markerWidth * view.overviewScale * scale);
-		int hMark=(int) (view.markerHeight * view.overviewScale * scale);
-		
-		if(x>xOver && x<xOver+wOver && y>yOver && y<yOver+hOver){
-			hitOverview=true;
-		}
-		
-		if(x>xMark && x<xMark+wMark && y>yMark && y<yMark+hMark){
-			hitMarker=true;
-			System.out.println("hitMarker: " + hitMarker);
-		}
-		
-		System.out.println("marker: " + xMark + "-" + yMark +"-" + wMark + "-" + hMark);
-		
-		
-	   if (edgeDrawMode){
-			drawingEdge = new DrawingEdge((Vertex)getElementContainingPosition(x/scale,y/scale));
+
+		if (edgeDrawMode) {
+			drawingEdge = new DrawingEdge((Vertex) getElementContainingPosition(x / scale, y / scale));
 			model.addElement(drawingEdge);
-		} else if (fisheyeMode){
+		} else if (fisheyeMode) {
 			/*
 			 * do handle interactions in fisheye mode
 			 */
 			view.repaint();
 		} else {
-			
-			selectedElement = getElementContainingPosition(x/scale,y/scale);
-			/*
-			 * calculate offset
-			 */
-			mouseOffsetX = x - selectedElement.getX() * scale ;
-			mouseOffsetY = y - selectedElement.getY() * scale ;	
+			double xTop = (view.overViewTop.getX() - view.getTranslateX()) * scale;
+			double yTop = (view.overViewTop.getY() - view.getTranslateY()) * scale;
+			double wTop = view.overViewTop.getWidth() * scale;
+			double hTop = view.overViewTop.getHeight() * scale;
+
+			double xMark = (view.getMarker().getX() - view.getTranslateX()) * scale;
+			double yMark = (view.getMarker().getY() - view.getTranslateY()) * scale;
+			double wMark = view.getMarker().getWidth() * scale;
+			double hMark = view.getMarker().getHeight() * scale;
+
+			if (x > xMark && x < xMark + wMark * scale && y > yMark && y < yMark + hMark * scale) {
+				hitMarker = true;
+			} else if (x > xTop && x < xTop + wTop * scale && y > yTop && y < yTop + hTop * scale) {
+				hitTopOverview = true;
+			}
+
+			System.out.println("Top " + hitTopOverview);
+			System.out.println("Marker" + hitMarker);
+
+			if (hitMarker || hitTopOverview) {
+				currentX = x;
+				currentY = y;
+			} else {
+				selectedElement = getElementContainingPosition(x / scale, y / scale);
+				/*
+				 * calculate offset
+				 */
+				mouseOffsetX = x - selectedElement.getX() * scale;
+				mouseOffsetY = y - selectedElement.getY() * scale;
+				currentX = 0;
+				currentY = 0;
+			}
 		}
-		
 	}
-	public void mouseReleased(MouseEvent arg0){
+
+	public void mouseReleased(MouseEvent arg0) {
 		int x = arg0.getX();
 		int y = arg0.getY();
-		
-		hitOverview=false; // overview
-		hitMarker=false; // marker
-		
-		if (drawingEdge != null){
+
+		hitTopOverview = false; // overview
+		hitMarker = false; // marker
+
+		if (drawingEdge != null) {
 			Element to = getElementContainingPosition(x, y);
-			model.addEdge(new Edge(drawingEdge.getFrom(),(Vertex)to));
+			model.addEdge(new Edge(drawingEdge.getFrom(), (Vertex) to));
 			model.removeElement(drawingEdge);
 			drawingEdge = null;
 		}
-		if (groupRectangle != null){
-		    Model groupedElements = new Model();
+		if (groupRectangle != null) {
+			Model groupedElements = new Model();
 			for (Iterator<Vertex> iter = model.iteratorVertices(); iter.hasNext();) {
 				Vertex vertex = iter.next();
-				if (groupRectangle.contains(vertex.getShape().getBounds2D())){
+				if (groupRectangle.contains(vertex.getShape().getBounds2D())) {
 					Debug.p("Vertex found");
-					groupedElements.addVertex(vertex);	
+					groupedElements.addVertex(vertex);
 				}
 			}
-			if (!groupedElements.isEmpty()){
+			if (!groupedElements.isEmpty()) {
 				model.removeVertices(groupedElements.getVertices());
-				
-				Vertex groupVertex = new Vertex(groupRectangle.getCenterX(),groupRectangle.getCenterX());
+
+				Vertex groupVertex = new Vertex(groupRectangle.getCenterX(), groupRectangle.getCenterX());
 				groupVertex.setColor(Color.ORANGE);
 				groupVertex.setGroupedElements(groupedElements);
 				model.addVertex(groupVertex);
-				
-				List<Edge> newEdges = new ArrayList(); 
+
+				List<Edge> newEdges = new ArrayList();
 				for (Iterator<Edge> iter = model.iteratorEdges(); iter.hasNext();) {
-					Edge edge =  iter.next();
-				    if (groupRectangle.contains(edge.getSource().getShape().getBounds2D()) 
-				    	&& groupRectangle.contains(edge.getTarget().getShape().getBounds2D())){
-				    		groupVertex.getGroupedElements().addEdge(edge);
-                            Debug.p("add Edge to groupedElements");	
-                            //iter.remove(); // Warum geht das nicht!
-				    } else if (groupRectangle.contains(edge.getSource().getShape().getBounds2D())){
-				    	groupVertex.getGroupedElements().addEdge(edge);
-				    	newEdges.add(new Edge(groupVertex,edge.getTarget()));
-				    } else if (groupRectangle.contains(edge.getTarget().getShape().getBounds2D())){
-				    	groupVertex.getGroupedElements().addEdge(edge);
-				    	newEdges.add(new Edge(edge.getSource(),groupVertex));
-				    }
+					Edge edge = iter.next();
+					if (groupRectangle.contains(edge.getSource().getShape().getBounds2D())
+							&& groupRectangle.contains(edge.getTarget().getShape().getBounds2D())) {
+						groupVertex.getGroupedElements().addEdge(edge);
+						Debug.p("add Edge to groupedElements");
+						// iter.remove(); // Warum geht das nicht!
+					} else if (groupRectangle.contains(edge.getSource().getShape().getBounds2D())) {
+						groupVertex.getGroupedElements().addEdge(edge);
+						newEdges.add(new Edge(groupVertex, edge.getTarget()));
+					} else if (groupRectangle.contains(edge.getTarget().getShape().getBounds2D())) {
+						groupVertex.getGroupedElements().addEdge(edge);
+						newEdges.add(new Edge(edge.getSource(), groupVertex));
+					}
 				}
 				model.addEdges(newEdges);
 				model.removeEdges(groupedElements.getEdges());
@@ -195,58 +203,65 @@ public class MouseController implements MouseListener,MouseMotionListener {
 		}
 		view.repaint();
 	}
-	
+
 	public void mouseDragged(MouseEvent e) {
 		int x = e.getX();
 		int y = e.getY();
 		double scale = view.getScale();
-		
-		if(hitMarker==true){
-			hitOverview=false;
-			view.updateMarker(x, y);
-			view.updateTranslation(x, y);
+		if (hitMarker || hitTopOverview) {
+			if (hitMarker) {
+				hitTopOverview = false;
+				double overviewScale = view.overviewScale;
+				double translateX = (x - currentX) * overviewScale + view.getTranslateX();
+				double translateY = (y - currentY) * overviewScale + view.getTranslateY();
+				view.updateTranslation(translateX, translateY);
+				currentX = x;
+				currentY = y;
+			} else {
+				double translateX = (x - currentX) / scale + view.getOverviewTranslateX();
+				double translateY = (y - currentY) / scale + view.getOverviewTranslateY();
+				view.setOverviewTranslateX(translateX);
+				view.setOverviewTranslateY(translateY);
+				currentX = x;
+				currentY = y;
+			}
 		}
-		
-		if(hitOverview==true){
-			view.updateOverview(x, y);
-		}
-		
-		/*
-		 * Aufgabe 1.2
-		 */
-		if (fisheyeMode){
+		if (fisheyeMode) {
 			/*
 			 * handle fisheye mode interactions
 			 */
 			fisheye.setMouseCoords(x, y, view); // Fisheye. Normal coords
-			fishModel=new Model();
-			for(int i=0;i<model.getVertices().size();i++){
+			fishModel = new Model();
+			for (int i = 0; i < model.getVertices().size(); i++) {
 				fishModel.addVertex(new Vertex(model.getVertices().get(i)));
 			}
-			fishModel=fisheye.transform(fishModel, view);
+			fishModel = fisheye.transform(fishModel, view);
 			view.setModel(fishModel);
-			
+
 			view.repaint();
-		} else if (edgeDrawMode){
+		} else if (edgeDrawMode) {
 			drawingEdge.setX(e.getX());
 			drawingEdge.setY(e.getY());
-		}else if(selectedElement != null){
-			selectedElement.updatePosition((e.getX()-mouseOffsetX)/scale, (e.getY()-mouseOffsetY) /scale);
+		} else if (selectedElement != null) {
+			selectedElement.updatePosition((e.getX() - mouseOffsetX) / scale, (e.getY() - mouseOffsetY) / scale);
 		}
 		view.repaint();
 	}
+
 	public void mouseMoved(MouseEvent e) {
 	}
+
 	public boolean isDrawingEdges() {
 		return edgeDrawMode;
 	}
+
 	public void setDrawingEdges(boolean drawingEdges) {
 		this.edgeDrawMode = drawingEdges;
 	}
-	
+
 	public void setFisheyeMode(boolean b) {
 		fisheyeMode = b;
-		if (b){
+		if (b) {
 			Debug.p("new Fisheye Layout");
 			/*
 			 * handle fish eye initial call
@@ -258,19 +273,19 @@ public class MouseController implements MouseListener,MouseMotionListener {
 			view.repaint();
 		}
 	}
-	
+
 	/*
 	 * private Methods
 	 */
-	private Element getElementContainingPosition(double x,double y){
+	private Element getElementContainingPosition(double x, double y) {
 		Element currentElement = new None();
 		Iterator<Element> iter = getModel().iterator();
 		while (iter.hasNext()) {
-		  Element element =  iter.next();
-		  if (element.contains(x, y)) currentElement = element;  
+			Element element = iter.next();
+			if (element.contains(x, y))
+				currentElement = element;
 		}
 		return currentElement;
 	}
-	
-    
+
 }
--- InfoVis_Assignment_SS15/src/infovis/diagram/View.java
+++ InfoVis_Assignment_SS15/src/infovis/diagram/View.java
@@ -1,143 +1,173 @@
 package infovis.diagram;
 
 import infovis.diagram.elements.Element;
+import infovis.diagram.elements.Vertex;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Rectangle2D;
-import java.util.Iterator;
-
-import java.awt.BasicStroke;
-import java.awt.Stroke;
 
 import javax.swing.JPanel;
 
-
-public class View extends JPanel{
+public class View extends JPanel {
 	private Model model = null;
 	private Color color = Color.BLUE;
 	private double scale = 1;
-	private double translateX= 0;
-	private double translateY=0;
+	private double translateX = 0;
+	private double translateY = 0;
 	private Rectangle2D marker = new Rectangle2D.Double();
-	Color markerColor = new Color(150, 150, 150, 127);
-	private Rectangle2D overviewRect = new Rectangle2D.Double();
-	private Rectangle2D dataRect = new Rectangle2D.Double();
-	
+	public Rectangle2D overviewRect = new Rectangle2D.Double();
+	public Rectangle2D overViewTop = new Rectangle2D.Double();
+
 	public int overviewX = 40; // overview
 	public int overviewY = 20; // overview
-	public double overviewScale = 0.2;
-	public double overviewWidth;
-	public double overviewHeight;
-	
+	public double overviewScale = 4;
+	private double overViewTranslateX = 0;
+	private double overViewTranslateY = 0;
+
 	public double markerX = 0; // marker
 	public double markerY = 0; // marker
-	public int markerWidth; //marker
+	public int markerWidth; // marker
 	public int markerHeight; // marker
-	
-	
-	
+
 	public Model getModel() {
 		return model;
 	}
+
 	public void setModel(Model model) {
 		this.model = model;
 	}
+
 	public Color getColor() {
 		return color;
 	}
+
 	public void setColor(Color color) {
 		this.color = color;
 	}
 
-	
 	public void paint(Graphics g) {
-		
+
 		Graphics2D g2D = (Graphics2D) g;
-		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2D.clearRect(0, 0, getWidth(), getHeight());
-		//g2D.translate(translateX * scale, translateY * scale);
-		//g2D.scale(scale, scale);
-		//paintDiagram(g2D);
-		
-		Graphics2D data = (Graphics2D) g;
-		data.scale(scale, scale);
-		dataRect.setRect(0, 0, overviewWidth, overviewHeight);
-		data.setColor(Color.darkGray);
-		data.translate(translateX, translateY);
-		data.draw(dataRect);
-		paintDiagram(data);
-		
-		
-		// Overview rectangle
-		overviewScale = 0.2/scale;
-		overviewWidth = getWidth();
-		overviewHeight = getHeight();
-		
-		Graphics2D overview = (Graphics2D) g;
-		overview.scale(overviewScale, overviewScale);
-		overviewRect.setRect(0, 0, overviewWidth, overviewHeight);
-		overview.setColor(Color.darkGray);
-		overview.translate(overviewX, overviewY);
-		overview.draw(overviewRect);
-		paintDiagram(overview);
-				
+
+		g2D.scale(scale, scale);
+		g2D.translate(-translateX, -translateY);
+		paintDiagram(g2D, model);
+
+		// Overview rectangle & top overview Rectangle
+		g2D.scale(1, 1);
+		g2D.setStroke(new BasicStroke((float) (1 / scale)));
+		double overviewTopY = translateY + overViewTranslateY;
+		double overviewTopHeight = 15 / scale;
+
+		double totalScale = overviewScale * scale;
+		double width = getWidth() / totalScale;
+		double height = getHeight() / totalScale;
+		double x = getWidth() / scale - width + translateX + overViewTranslateX;
+		double y = overviewTopY + overviewTopHeight;
+
+		overViewTop.setRect(x, overviewTopY, width, overviewTopHeight);
+		g2D.fill(overViewTop);
+		g2D.draw(overViewTop);
+
+		overviewRect.setRect(x, y, width, height);
+		g2D.draw(overviewRect);
+		g2D.setPaint(Color.white);
+		g2D.fill(overviewRect);
+
+		// draw a scaled model into the overview rectangle
+		g2D.translate(x, y); // translate to overview rectangle's coordinate system
+		Model scaledModel = new Model();
+		for (Element element : model.getElements()) {
+			Vertex vertex = (Vertex) element;
+			Vertex newElement = new Vertex(vertex.getX() / totalScale, vertex.getY() / totalScale,
+					vertex.getWidth() / totalScale, vertex.getHeight() / totalScale);
+			scaledModel.addElement((Element) newElement);
+		}
+		paintDiagram(g2D, scaledModel);
+		g2D.translate(-x, -y); // translate back to original cooordinate system
+
 		// Marker rectangle
 		markerWidth = (int) (getWidth() / scale);
 		markerHeight = (int) (getHeight() / scale);
-		
-		Graphics2D markerView = (Graphics2D) g;
-		marker.setRect(markerX, markerY, markerWidth, markerHeight);
-		markerView.setColor(markerColor);
-		markerView.fill(marker);
-		markerView.draw(marker);
-		
+
+		double differenceX = (translateX / scale) / overviewScale;
+		double differenceY = (translateY / scale) / overviewScale;
+		double markerWidth = overviewRect.getWidth() / this.scale;
+		double markerHeight = overviewRect.getHeight() / this.scale;
+		double markerX = overviewRect.getX() + differenceX;
+		double markerY = overviewRect.getY() + differenceY;
+		marker = new Rectangle2D.Double(markerX, markerY, markerWidth, markerHeight);
+		g2D.setColor(Color.MAGENTA);
+		g2D.draw(marker);
+
 	}
-	
-	private void paintDiagram(Graphics2D g2D){
-		for (Element element: model.getElements()){
+
+	private void paintDiagram(Graphics2D g2D, Model model) {
+		for (Element element : model.getElements()) {
 			element.paint(g2D);
 		}
 	}
-	
+
 	public void setScale(double scale) {
 		this.scale = scale;
 	}
-	public double getScale(){
+
+	public double getScale() {
 		return scale;
 	}
+
 	public double getTranslateX() {
 		return translateX;
 	}
+
 	public void setTranslateX(double translateX) {
 		this.translateX = translateX;
 	}
+
 	public double getTranslateY() {
 		return translateY;
 	}
+
 	public void setTranslateY(double tansslateY) {
 		this.translateY = tansslateY;
 	}
-	public void updateTranslation(double x, double y){
+
+	public double getOverviewTranslateX() {
+		return overViewTranslateX;
+	}
+
+	public void setOverviewTranslateX(double x) {
+		overViewTranslateX = x;
+	}
+
+	public double getOverviewTranslateY() {
+		return overViewTranslateY;
+	}
+
+	public void setOverviewTranslateY(double y) {
+		overViewTranslateY = y;
+	}
+
+	public void updateTranslation(double x, double y) {
 		setTranslateX(x);
 		setTranslateY(y);
-	}	
-	public void updateMarker(int x, int y){
-		//marker.setRect(x, y, markerWidth, markerHeight);
-		markerX= (int) (x / overviewScale / scale);
-		markerY= (int) (y / overviewScale / scale);
 	}
-	public Rectangle2D getMarker(){
+
+	public void updateMarker(int x, int y) {
+		marker.setRect(x, y, 16, 10);
+	}
+
+	public Rectangle2D getMarker() {
 		return marker;
 	}
-	public boolean markerContains(int x, int y){
+
+	public boolean markerContains(int x, int y) {
 		return marker.contains(x, y);
 	}
-	public void updateOverview(int x, int y) { // overview
-		overviewX = (int) (x / overviewScale / scale);
-		overviewY = (int) (y / overviewScale / scale);
-	}
 }
- 
